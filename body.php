053fa6df32cd58346b68c11c77712e6fcfe1f3dd
taylorsuccessor <taylorsuccessor@gmail.com>
2016-04-19 17:59:45 +0300

    body

--- body.php
+++ body.php
@@ -27,10 +27,51 @@
             </div>
         </div>
         <div class="col-xs-7">
-            
+
+
+            <div class="col-xs-12 oneGroup mainGroup">
+                <span class=""><img src="images/logo.png"></span>
+                <h6>MAPS INVESTMENT GROUP LTD</h6>
+                <p>MAPS IG is a Leading Online Forex and Commodity Broker focusing on providing premium brokerage services via its Meta Trader 4 trading system.
+                </p>
+            </div>
+
+
+
+            <div class="col-xs-6 oneGroup mainGroup">
+                <span class="fa fa-users"></span>
+                <h6>Trade Major Markets</h6>
+                <p>Total transparency and security of the world's leading financial centers </p>
+            </div>
+
+
+
+
+
+            <div class="col-xs-6 oneGroup mainGroup">
+                <span class="fa fa-users"></span>
+                <h6>Trade Major Markets</h6>
+                <p>Total transparency and security of the world's leading financial centers </p>
+            </div>
+
+
+            <div class="col-xs-6 oneGroup mainGroup">
+                <span class="fa fa-users"></span>
+                <h6>Trade Major Markets</h6>
+                <p>Total transparency and security of the world's leading financial centers </p>
+            </div>
+
+
+            <div class="col-xs-6 oneGroup mainGroup">
+                <span class="fa fa-users"></span>
+                <h6>Trade Major Markets</h6>
+                <p>Total transparency and security of the world's leading financial centers </p>
+            </div>
+
 
         </div>
 
     </div>
 
+    <div class="row shadow"></div>
 </div>
