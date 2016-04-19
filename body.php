07d1deb1e06f34703dada13138ac868a7b8230aa
taylor <taylorsuccessor@gmail.com>
2016-04-19 23:53:35 +0300

    home page

--- body.php
+++ body.php
@@ -26,7 +26,7 @@
             <img src="images/tradeImgWithLogin.png">
             </div>
         </div>
-        <div class="col-xs-7">
+        <div class="col-xs-7" id="rightGroupsContainer">
 
 
             <div class="col-xs-12 oneGroup mainGroup">
@@ -75,3 +75,113 @@
 
     <div class="row shadow"></div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="row">
+    <div class="container">
+
+
+        <div class="col-xs-4">
+            <div class="oneDescriptionWithButton">
+                <img src="images/logo.png">
+                <h3>CFD Trading</h3>
+                <p>A contract for differences, or simply a CFD, is one of the world's fastest
+                growing and exciting trading instruments.</p>
+                <a class="button primaryButton thickButton">LEARN MORE</a>
+            </div>
+        </div>
+
+
+
+
+        <div class="col-xs-4">
+            <div class="oneDescriptionWithButton">
+                <img src="images/logo.png">
+                <h3>CFD Trading</h3>
+                <p>A contract for differences, or simply a CFD, is one of the world's fastest
+                    growing and exciting trading instruments.</p>
+                <a class="button primaryButton thickButton">LEARN MORE</a>
+            </div>
+        </div>
+
+
+
+
+        <div class="col-xs-4">
+            <div class="oneDescriptionWithButton">
+                <img src="images/logo.png">
+                <h3>CFD Trading</h3>
+                <p>A contract for differences, or simply a CFD, is one of the world's fastest
+                    growing and exciting trading instruments.</p>
+                <a class="button primaryButton thickButton">LEARN MORE</a>
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<div class="row bgCover" id="softwareBanner">
+    <div class="container">
+    <div class="innerContainer">
+        <h4>Smartphone, Tablet & PC Trading</h4>
+        <p>Trade anywhere, any time at a place convenient for you.</p>
+
+        <a href="#" class="button primaryButton">
+            metatrader4<br> for pc
+        </a>
+
+        <a href="#" class="button whiteButton">
+            metatrader4<br> for iphone
+        </a>
+
+        <a href="#" class="button blueButton">
+            metatrader4<br> for android
+        </a>
+
+    </div>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+<div class="row">
+    <div class="centerTitleText">
+        <h3>trading account types&  conditions</h3>
+        
+    </div>
+
+</div>
+
